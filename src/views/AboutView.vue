a00da5eb1dec0f2586e215327766ddb8143c2ea1
lum-jashari <lumijashari@hotmail.com>
2024-02-29 17:38:57 +0100

    pinia store counter app finished

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,15 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "@/stores/counter";
+
+const store = useCounterStore();
+const { count } = storeToRefs(store);
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h2>{{ count }}</h2>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "@/stores/counter";
+
+const store = useCounterStore();
+const { count, doubleCount } = storeToRefs(store);
+const { increment, decrement } = store;
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>{{ count }}</h1>
+    <div>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
+    <h2>doubled: {{ doubleCount }}</h2>
+    <input type="number" v-model="count" />
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  width: 3rem;
+  height: 3rem;
+  font-weight: bold;
+  font-size: 1.5rem;
+  border-radius: 0.5em;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+}
+input {
+  padding: 1em;
+  margin-top: 1em;
+  border-radius: 0.5em;
+  font-size: 1rem;
+}
+</style>
